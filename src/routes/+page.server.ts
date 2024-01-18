67b1943164a817e8da33c6bf9e52b1d4bb210ce1
Erik Dickens <erik.dickens@gmail.com>
2024-01-18 02:09:27 +0100

    changed execution time

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,7 @@ import type { Actions } from '@sveltejs/kit';
 
 export const actions: Actions = {
 	callFunction: async () => {
-		await new Promise((resolve) => setTimeout(resolve, 3000));
+		await new Promise((resolve) => setTimeout(resolve, 20000));
 		return {
 			message: 'Function executed!'
 		};
